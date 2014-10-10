e006bf9b55fe0b7f649598343b8016561e3c53b9
messense <messense@icloud.com>
2014-10-10 17:59:12 +0800

    Bug fix

--- image2video.py
+++ image2video.py
@@ -12,9 +12,10 @@ DEFAULT_VIDEO_SECONDS = 9
 def add_alpha_channel(image, alpha=255):
     for i in range(image.width):
         for j in range(image.height):
-            channels = cv.Get2D(image, j, i)
+            channels = list(cv.Get2D(image, j, i))
             channels[3] = alpha
             cv.Set2D(image, j, i, channels)
+    return image
 
 
 class FrameFilter(object):
