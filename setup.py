b890765d567235f915dc45efdef157e0e6331505
messense <messense@icloud.com>
2014-10-13 10:01:35 +0800

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(readme) as f:
 
 setup(
     name='images2video',
-    version='0.1',
+    version='0.2',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/messense/images2video',
